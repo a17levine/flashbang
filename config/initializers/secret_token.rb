64bcf6acd529c87d5aa1bb1ec8d3ca4cbeb6d6c6
Alex Levine <a17levine@gmail.com>
2013-09-05 16:43:01 -0500

    initial commit with rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flashbang::Application.config.secret_key_base = 'fa4e080dbc5fe8476ef1a2d5c101f326f3f92c0e4d3b32f66d023fbbac482545dda8aeb3cc0a00527b9e9e404f900cd09649ed3fdf299f2d516140bee34aabbf'
