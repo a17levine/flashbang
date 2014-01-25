39500461150a71514bf5077518f58cdc5f805405
Alex Levine <a17levine@gmail.com>
2014-01-25 16:59:44 -0600

    added styling to sign in page, new post page, completed logout functionality, enforced authentication in the post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,16 @@
 class PostsController < ApplicationController
 
-
+	before_filter :authenticate_user!
 
 	# Latest posts index
 	def latest
 		@latest_posts = Post.order("created_at DESC")
+		# if current_user
 		@user = current_user
 		@post = current_user.posts.new
+		# else
+		# 	redirect_to new_user_session_path
+		# end
 	end
 
 	# Top posts index
