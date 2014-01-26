6f0bb3cd80dfa983bee77b1a40079ec9e87b3345
Alex Levine <a17levine@gmail.com>
2014-01-26 00:56:46 -0600

    started post show. need to start thinking about tagging, user sign-up, tagging, and landing page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Flashbang::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'posts#latest'
 
+  get 'posts/:id' => 'posts#show', :as => "post"
+
   resources :users do
     resources :posts  
   end
---
+++ notes.md
@@ -0,0 +1,5 @@
+I can query facebook for a user's network by submitting this FQL query to Facebook Graph API
+
+`select affiliations from user WHERE uid = me()`
+
+substituting me() for the uid of the user
