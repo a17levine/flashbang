08c26d6c165a14bd043498714ded445308466fbc
Alex Levine <a17levine@gmail.com>
2014-04-05 23:05:35 -0500

    Fixed a bug dealing with an array not clearing on zero elements

--- app/assets/javascripts/tagging.js
+++ app/assets/javascripts/tagging.js
@@ -3,10 +3,7 @@ var tags = [];
 var updateHiddenInputWithTagChange = function (event, ui) {
   var tags = [];
   $(".tagit-label").each(function(i,v){tags.push($(v).text())})
-
-  if (tags.length > 0){
-    $("#post_tags").attr("value", JSON.stringify(tags));
-  }
+  $("#post_tags").attr("value", JSON.stringify(tags));
 };
 
 
