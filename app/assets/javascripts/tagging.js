8cc73b3a27eb32fd425c88407ab2a73237f856db
Alex Levine <a17levine@gmail.com>
2014-04-05 21:54:27 -0500

    got tags to be in JSON, now need to send to the server

--- app/assets/javascripts/tagging.js
+++ app/assets/javascripts/tagging.js
@@ -1,5 +1,23 @@
+var tags = [];
+
+var updateHiddenInputWithTagChange = function (event, ui) {
+  // console.log(ui.tag.text().slice(0, -1));
+  tags.push(ui.tag.text().slice(0, -1))
+  console.log("Tags array is:");
+  console.log(tags);
+  if (tags.length > 0){
+    console.log("Post tags are:");
+    $("#post_tags").text(JSON.stringify(tags));
+    console.log($("#post_tags").text());
+  }
+};
+
+
 $("#mytags").ready(function() {
   $("#mytags").tagit({
+
+    afterTagRemoved: updateHiddenInputWithTagChange,
+    afterTagAdded: updateHiddenInputWithTagChange
 
   // // This is for AJAX stuff - not there yet
 
