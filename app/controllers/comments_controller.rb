d1a93623531eabefa9a3767411772199c4bc25df
Alex Levine <a17levine@gmail.com>
2014-04-13 16:26:17 -0500

    added some spacing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,7 @@ class CommentsController < ApplicationController
     comment_text = params[:comment][:text]
     post = Post.find(params[:post_id])
     user_id = params[:user_id].to_i
+    
     post.comments.create(user_id: user_id, text: comment_text)
     redirect_to post
   end
