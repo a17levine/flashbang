fd44a904c9a8d33448d5ef4161e6e86321126dda
Nick McDonnough <nickmcd@gmail.com>
2014-05-01 18:43:07 -0500

    Add validations for offer model.

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,10 @@
 class Offer < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
+
+  validates_presence_of :user_id, :value
+  validates :value, :numericality => { :greater_than => 0, :integer => true }
+
+
+
 end
