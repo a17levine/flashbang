f760a5ed5219c1c79e976c2dc148deab492f5bf7
Nick McDonnough <nickmcd@gmail.com>
2014-05-03 00:35:09 -0500

    Add validation to check that a new offer is higher than the last.

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,7 +4,23 @@ class Offer < ActiveRecord::Base
 
   validates_presence_of :user_id, :amount
   validates :amount, :numericality => { :greater_than => 0, :integer => true }
+  validate :must_be_highest_offer
 
-  before_create {|offer| offer.accepted = false; true}
+  before_create { |offer| offer.accepted = false; true }
 
+
+  def must_be_highest_offer
+    amount = self.amount
+    post = Post.find(self.post_id)
+    offers = post.offers.sort_by(&:amount)
+    
+    return true if offers.empty?
+
+    if self.amount > offers.last.amount
+      return true
+    else
+      # self.errors[:amount] << "Your offer was not higher than the last offer!"
+      return false
+    end
+  end
 end
