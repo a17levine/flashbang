d7cf3ad14e0d2b4c4c762eb50568ed4ecb7ded81
Alex Levine <a17levine@gmail.com>
2014-05-09 11:32:21 -0700

    failed to write a test again but i should ask mike ornellas

--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -2,7 +2,23 @@ require 'spec_helper'
 
 describe Offer do
   
-	it "should be higher than the current highest offer"
+	# it "should be higher than the current highest offer"
+
+	it "should make its post inactive after an offer 
+		on the post is accepted" do
+
+		# ugh I don't know what I'm doing. Commenting this all out -Alex
+			
+			# post = Post.new
+			# userCreatingPost = User.new(first_name: "Bob", last_name: "Smith")
+			# userOffering = User.new(first_name: "Sue", last_name: "Jones")
+			# offer = post.offers.new(user_id:userOffering.id, amount: 56)
+			# offer.accepted = true
+			# invalidOffer = post.offers.new(user_id:userOffering.id, amount: 60)
+
+			# expect(invalidOffer).to be_invalid
+
+		end
 
 end
 
