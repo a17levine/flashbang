53ec1f6c1efa276f9764bc239ea14662db2394e2
Alex Levine <a17levine@gmail.com>
2014-05-26 13:55:28 -0500

    generated exchanges controller, wrote pseudo code

---
+++ app/controllers/exchanges_controller.rb
@@ -0,0 +1,20 @@
+class ExchangesController < ApplicationController
+
+	def create
+		# get buyer and seller objects
+		# create a new exchange with them as buyers and sellers
+		# trigger a notice congratulating the seller for accepting
+		# render show
+	end
+
+	def show
+		# get exchange from db
+		# @exchange = Exchange.find(params[])
+		# load objects
+		# @buyer = exchange.buyer
+		# @seller = exchange.seller
+		# security: make sure only buyer or seller is viewing.
+		#   otherwise return to root path with a message
+	end
+
+end
