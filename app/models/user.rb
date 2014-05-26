d3492568ecd3ec7d12e15c82f6f9dd609757fcb9
Alex Levine <a17levine@gmail.com>
2014-05-26 17:41:54 -0500

    model methods on user for following tags

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,14 @@ class User < ActiveRecord::Base
 	 	"#{self.first_name} #{self.last_name}"
 	 end
 
+   def follow_tag(tag_text)
+     self.followed_tags.create(tag: Tag.find_or_create_by_name(tag_text))
+   end
+
+   def unfollow_tag(tag_text)
+     self.followed_tags.where(user_id: self.id).where(tag_id: Tag.find_by(name: tag_text)).first.delete
+   end
+
    def exchanges
      (self.selling_exchanges + self.buying_exchanges).sort_by(&:created_at)
    end
