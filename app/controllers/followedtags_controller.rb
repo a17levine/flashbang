dee17a0a9b329a39c82b2d35c757035eb51289bb
Alex Levine <a17levine@gmail.com>
2014-06-22 22:12:04 -0500

    got follow tags to work, now just need unfollow to work

--- app/controllers/followedtags_controller.rb
+++ app/controllers/followedtags_controller.rb
@@ -1,12 +1,15 @@
 class FollowedtagsController < ApplicationController
 
   def index
+    @relevant_posts = Array.new
   	if current_user.followed_tags.empty?
   		redirect_to followed_tags_show_path, :flash => { :notice => "you must follow tags to view posts you're following" }
   	else
-  		current_user.tags.each do |t|
-      	Post.tagged_with(t.name).where(active: true).sort_by(&:created_at)
+      current_user.tags.each do |t|
+      	posts_with_tag = Post.tagged_with(t.name).where(active: true).sort_by(&:created_at)
+        posts_with_tag.each {|p| @relevant_posts << p}
     	end
+      @relevant_posts.uniq!
   	end
     
   end
