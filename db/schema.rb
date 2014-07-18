ba074db0ccb30063e8bd35f33d3b6c83c584e714
Alex Levine <a17levine@gmail.com>
2014-07-18 18:43:42 -0500

    got correct variables from google JS
    now need to post to the server

--- db/schema.rb
+++ db/schema.rb
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 20140608175508) do
   create_table "places", force: true do |t|
     t.string   "name"
     t.string   "url"
-    t.string   "address"
+    t.string   "address"  
     t.datetime "created_at"
     t.datetime "updated_at"
   end
