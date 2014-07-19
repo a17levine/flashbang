7601d2fd6343c4ceee98a6dd107c0cb495a294c6
Alex Levine <a17levine@gmail.com>
2014-07-19 17:20:09 -0500

    small map edits

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,7 +3,7 @@ class Place < ActiveRecord::Base
 
 	def static_map
 		if self.address
-			map = GoogleStaticMap.new(:zoom => 13, :center => MapLocation.new(:address => self.address), :width => 640, :height => 600)
+			map = GoogleStaticMap.new(:zoom => 13, :center => MapLocation.new(:address => self.address), :width => 640, :height => 400)
 			map.markers << MapMarker.new(:color => "blue", :location => MapLocation.new(:address => self.address))
 			map.url
 		else
