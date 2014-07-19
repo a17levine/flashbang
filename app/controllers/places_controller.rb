98b1b5168c81efb2143c601b232407be7e4d72e2
Alex Levine <a17levine@gmail.com>
2014-07-19 16:17:36 -0500

    added first or create method

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,14 +1,17 @@
 class PlacesController < ApplicationController
   def create
   	# binding.pry
-  	@exchange = Exchange.find(params[:exchange][:id])
+  	@exchange = Exchange.find(params[:exchange][:id].to_i)
   	@name = params[:place][:name]
   	@address = params[:place][:address]
   	@url = params[:place][:url]
   	@place = Place.new(name: @name, address: @address, url: @url)
-  	
-  	if @exchange.seller == current_user && @place.save
-  		@exchange.place_id = @place
+  
+  	if @exchange.seller == current_user
+  		@place = Place.where(:url => @url).first_or_create do |p|
+  			p.attributes = {name: @name, address: @address, url: @url}
+  		end
+  		@exchange.place_id = @place.id
   		@exchange.save
   		redirect_to "#{exchange_path(@exchange)}#place", :flash => { :notice => "location updated" }
   	end
