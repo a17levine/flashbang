effc3710647dd02f59f433869605025abaef3780
Alex Levine <a17levine@gmail.com>
2014-07-19 16:58:52 -0500

    got static maps installed

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,13 @@
 class Place < ActiveRecord::Base
 	has_many :exchanges
+
+	def static_map
+		if self.address
+			map = GoogleStaticMap.new(:zoom => 13, :center => MapLocation.new(:address => self.address), :width => 640, :height => 600)
+			map.markers << MapMarker.new(:color => "blue", :location => MapLocation.new(:address => self.address))
+			map.url
+		else
+			nil
+		end
+	end
 end
